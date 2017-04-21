ee648c9e616de211a3d37e703e4a406d3f9bb888
jun.chu <1571970439@qq.com>
2017-04-21 11:22:27 +0800

    init project

---
+++ core/src/main/java/com/fuiou/gpay/App.java
@@ -0,0 +1,13 @@
+package com.fuiou.gpay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
