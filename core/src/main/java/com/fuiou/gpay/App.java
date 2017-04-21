05a08bdd6a522e3d1d72405677defcf03a5795d2
jun.chu <1571970439@qq.com>
2017-04-21 11:36:15 +0800

    add JsonUtil test

--- core/src/main/java/com/fuiou/gpay/App.java
+++ core/src/main/java/com/fuiou/gpay/App.java
@@ -1,5 +1,10 @@
 package com.fuiou.gpay;
 
+import com.fuiou.gpay.utils.JsonUtils;
+import com.sun.tools.javac.util.List;
+
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("1");
+        list.add("2");
+        System.out.println(JsonUtils.fromJsonList(JsonUtils.toJson(list),String.class));
     }
 }
