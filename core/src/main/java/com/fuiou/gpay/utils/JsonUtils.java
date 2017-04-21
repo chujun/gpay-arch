1ce2ba7edc4d44ffaca258da5bbe7d4d369a2623
jun.chu <1571970439@qq.com>
2017-04-21 11:30:11 +0800

    add JsonUtils

---
+++ core/src/main/java/com/fuiou/gpay/utils/JsonUtils.java
@@ -0,0 +1,49 @@
+package com.fuiou.gpay.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
+/**
+ * Created by chujun on 2017/3/10.
+ */
+public class JsonUtils {
+    private static final Gson gson = new Gson();
+
+    /**
+     * 对象转化为json对象
+     *
+     * @param t
+     * @param <T>
+     * @return
+     */
+    public static <T> String toJson(T t) {
+        return gson.toJson(t);
+    }
+
+    /**
+     * json字符串转化为指定类型对象
+     *
+     * @param str
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T fromJson(String str, Class<T> clazz) {
+        return gson.fromJson(str, clazz);
+    }
+
+    /**
+     * json字符串创转化为指定类型对象列表
+     *
+     * @param jsonStr json字符串
+     * @param tClass
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> fromJsonList(String jsonStr, Class<T> tClass) {
+        return gson.fromJson(jsonStr, new TypeToken<List<T>>() {
+        }.getType());
+    }
+}
